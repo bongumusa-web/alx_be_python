5cc9314c84d478326996fef93c8e3ccb6b4fcfa2
Bongumusa Gumede <bonggums@gmail.com>
2025-06-01 15:44:41 +0200

    control-flow/

--- control-flow/pattern_drawing.py
+++ control-flow/pattern_drawing.py
@@ -1,1 +1,12 @@
+pattern = int(input("Enter the size of the pattern: "))
+row = pattern
+row_count = pattern
+
+while  row_count > 0:
+    
+    for i in range(row):
+        print("*", end="")
+
+    print()
+    row_count -= 1
 
