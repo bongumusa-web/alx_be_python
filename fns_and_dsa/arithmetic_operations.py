ad90f0647b33839b86d789a644a6d1910fb76e74
Bongumusa Gumede <bonggums@gmail.com>
2025-06-08 20:52:37 +0200

    arithmetic_operations.py

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,1 +1,14 @@
-
+def perfom_operation(num1,num2, operation):
+    if operation == 'add':
+        return num1 + num2
+    elif operation == 'subtract':
+        return num1 - num2
+    elif operation == 'multiply':
+        return num1 * num2
+    elif operation == 'divide':
+        if num2 != 0:
+            return num1 / num2
+        else:
+            return "Error: Division by zero is not allowed."
+    else:
+        return "Error: Invalid operation. Please choose from add, subtract, multiply, or divide."
