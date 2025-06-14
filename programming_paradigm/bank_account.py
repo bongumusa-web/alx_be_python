43530001880cc549581c566f07ff7e8368847699
bongumusa-web <bonggums@gmail.com>
2025-06-14 20:41:01 +0200

    Update bank_account.py

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -1,19 +1,20 @@
 class BankAccount:
-    def __init__(self,account_balance):
+    def __init__(self, account_balance):
         self.account_balance = 0 # account balance
+        
 
     def deposit(self, amount):
         self.amount = amount
-        self.account_balance += self.amount
-        return self.account_balance
+        self.account_balance = self.account_balance + self.amount
+        return True
 
     def withdraw(self, amount):
         self.amount = amount
         self.account_balance = self.account_balance - self.amount
         if self.account_balance > self.amount :
             return True
-        elif self.account_balance <self.amount :
+        elif self.account_balance < self.amount :
             return False
 
     def display_balance(self):
-        print(f"Your current balance is: ${self.account_balance:.2f}" )
+        print(f"Current Balance: ${self.account_balance:.2f}" )
