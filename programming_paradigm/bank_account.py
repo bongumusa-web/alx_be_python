dad06ba7e89274ca0962516e4d83ebe21b422d3f
bongumusa-web <bonggums@gmail.com>
2025-06-14 20:56:09 +0200

    Update bank_account.py

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -4,7 +4,6 @@ class BankAccount:
 
     def deposit(self, amount):
         self.account_balance += amount
-        print(f"Deposited: ${amount}")
         return True
 
     def withdraw(self, amount):
