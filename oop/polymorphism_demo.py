f33eb3f61df8902b8a8387244504e1b2fc437462
bongumusa-web <bonggums@gmail.com>
2025-06-22 21:10:19 +0200

    Update polymorphism_demo.py

--- oop/polymorphism_demo.py
+++ oop/polymorphism_demo.py
@@ -1,1 +1,24 @@
+import math
+
+class Shape:
+    def area(self):
+        raise NotImplementedError
+
+class Rectangle(Shape):
+    def __init__(self, length, width):
+        self.length =length
+        self.width = width
+
+    def area(self):
+        area = self.length * self.width
+        return area
+
+
+class Circle(Shape):
+    def __init__(self, radius):
+        self.radius = radius
+
+    def area(self):
+        area = math.pi * math.pow(self.radius, 2)
+        return area
 
